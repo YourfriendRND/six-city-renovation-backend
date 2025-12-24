f9617cdd10690caecf8c8eaea368def058f8e53a
YourfriendRND <kupeirc@gmail.com>
2025-12-24 11:21:48 +0300

    fix: Исправил команды миграций

--- apps/core-service/src/data-source.ts
+++ apps/core-service/src/data-source.ts
@@ -1,22 +1,34 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { join } from 'path';
-import { ConfigService, ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 
-ConfigModule.forRoot();
+dotenv.config({
+  path: '.env',
+});
 
-const configService = new ConfigService();
+/**
+ * __dirname в src - apps/core-service/src
+ * __dirname в dist - dist/apps/core-service/src
+ */
+const rootDir = __dirname;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
-  username: configService.get('DB_USERNAME'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_NAME'),
+  host: process.env['DB_HOST'],
+  port: Number(process.env['DB_PORT']),
+  username: process.env['DB_USERNAME'],
+  password: process.env['DB_PASSWORD'],
+  database: process.env['DB_NAME'],
+
   synchronize: false,
   logging: false,
-  entities: [join(__dirname, '/modules/**/*.entity{.ts,.js}')],
-  migrations: [join(__dirname, '/migrations/*{.ts,.js}')],
-  cache: false,
+
+  entities: [
+    join(rootDir, '../**/*.entity.{js,ts}'),
+  ],
+
+  migrations: [
+    join(rootDir, './migrations/*.{js,ts}'),
+  ],
 });
